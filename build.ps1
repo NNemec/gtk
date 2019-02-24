1e61ae199298d4839811f6c6b7eb42598a16f2e8
Norbert Nemec <Norbert@Nemec-online.de>
2019-02-24 07:38:01 +0100

    refine build.ps1

--- build.ps1
+++ build.ps1
@@ -6,8 +6,13 @@ function meson {
     python3 ..\meson\meson.py @params
 }
 
-New-Item .\.builddir -ItemType Directory -ErrorAction SilentlyContinue
+#meson configure -Dintrospection=false # pango does not seem to compile with this...
+#meson configure -Dpango:gir=false
 
-meson wrap promote subprojects\glib\subprojects\libffi.wrap
-meson wrap promote subprojects\glib\subprojects\zlib.wrap
+#meson wrap promote subprojects\glib\subprojects\libffi.wrap
+#meson wrap promote subprojects\glib\subprojects\zlib.wrap
+#meson wrap promote subprojects\glib\subprojects\proxy-libintl.wrap
+
+#meson wrap promote subprojects\pango\subprojects\fribidi.wrap
+
 meson .\builddir
