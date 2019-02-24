7ea867501b6f5c48eb31001ff252ca08827bf91a
Norbert Nemec <Norbert@Nemec-online.de>
2019-02-24 09:26:36 +0100

    deactivate introspection/gir

--- build.ps1
+++ build.ps1
@@ -6,13 +6,4 @@ function meson {
     python3 ..\meson\meson.py @params
 }
 
-#meson configure -Dintrospection=false # pango does not seem to compile with this...
-#meson configure -Dpango:gir=false
-
-#meson wrap promote subprojects\glib\subprojects\libffi.wrap
-#meson wrap promote subprojects\glib\subprojects\zlib.wrap
-#meson wrap promote subprojects\glib\subprojects\proxy-libintl.wrap
-
-#meson wrap promote subprojects\pango\subprojects\fribidi.wrap
-
-meson .\builddir
+meson .\builddir -Dintrospection=false "-Dpango:gir=false"
